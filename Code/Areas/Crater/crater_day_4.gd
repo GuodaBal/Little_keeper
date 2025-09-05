fe09d2e8b75025da9e610d7e705ea11ea51c6a92
GuodaBal <az.guodabaltutyte@gmail.com>
2025-09-05 18:52:18 +0300

    ending_script_change

--- Code/Areas/Crater/crater_day_4.gd
+++ Code/Areas/Crater/crater_day_4.gd
@@ -4,12 +4,19 @@ func _ready() -> void:
 	GlobalSignals.start_ending.connect(start_ending)
 	
 func start_ending():
-	if GlobalVariables.ending_choice == "crowned":
+	if GlobalVariables.ending_choice == "crowned" && GlobalVariables.given_item_types.count(3) >= 3:
 		await DialogueManager.dialogue_ended
 		DialogueManager.show_dialogue_balloon(load("res://Resources/Dialogue/Cutscenes/endings.dialogue"), "crowned_ending")
 		GlobalSignals.player_can_move.emit(false)
 		await DialogueManager.dialogue_ended
 		var instance = load("res://Code/UI/Title_cards/title_card_crowned.tscn").instantiate()
+		add_child(instance)
+	elif GlobalVariables.ending_choice == "crowned":
+		await DialogueManager.dialogue_ended
+		DialogueManager.show_dialogue_balloon(load("res://Resources/Dialogue/Cutscenes/endings.dialogue"), "tended_ending")
+		GlobalSignals.player_can_move.emit(false)
+		await DialogueManager.dialogue_ended
+		var instance = load("res://Code/UI/Title_cards/title_card_tended.tscn").instantiate()
 		add_child(instance)
 	elif GlobalVariables.ending_choice == "bound" && GlobalVariables.given_item_types.count(3) >= 2:
 		await DialogueManager.dialogue_ended
