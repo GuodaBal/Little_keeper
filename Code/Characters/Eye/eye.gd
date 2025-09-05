ab478f33145f0137eedb8c05d7ce893ced303b8b
GuodaBal <az.guodabaltutyte@gmail.com>
2025-09-05 22:51:12 +0300

    testing_time

--- Code/Characters/Eye/eye.gd
+++ Code/Characters/Eye/eye.gd
@@ -1,6 +1,35 @@
 extends StaticBody2D
 
+@onready var giant_eye := $GiantEye
+@onready var large_eye := $LargeEye
+@onready var small_eye := $SmallEye
+
 var dm = DialogueManager
+
+func _ready() -> void:
+	match GlobalVariables.current_day:
+		1:
+			giant_eye.visible = false
+			large_eye.visible = false
+			small_eye.visible = false
+		2:
+			giant_eye.visible = false
+			large_eye.visible = false
+			small_eye.visible = true
+			for child in small_eye.get_children():
+				child.play("idle")
+		3:
+			giant_eye.visible = false
+			small_eye.visible = false
+			large_eye.visible = true
+			for child in large_eye.get_children():
+				child.play("idle")
+		4:
+			giant_eye.visible = true
+			small_eye.visible = false
+			large_eye.visible = true
+			for child in giant_eye.get_children():
+				child.play("idle")
 
 func interact():
 	if GlobalVariables.eye_is_fed:
